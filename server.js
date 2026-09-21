3df07d7c0c3e6b4a0528a63a905605dbf7bb7a94
stevehamby1978-bit <stevehamby1978@gmail.com>
2026-09-20 22:09:14 -0500

    Add copyright notice to server.js
    Added copyright notice and proprietary information.

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-
+/*
+ * S&K Auto Shop Management System
+ * Copyright © 2026 S&K Auto. All Rights Reserved.
+ *
+ * This software and its source code are proprietary to S&K Auto.
+ * Unauthorized copying, modification, distribution, or commercial use
+ * is prohibited except with permission from the copyright owner.
+ */
 const express = require('express');
 const Database = require('better-sqlite3');
 const path = require('path');
